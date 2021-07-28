96f35023db5f93c01456eb83e5089b21e7ec8b76
Simon <simonwatson176@gmail.com>
2021-07-28 19:32:40 +0100

    last test passed

--- src/tests/Calculator.test.js
+++ src/tests/Calculator.test.js
@@ -91,7 +91,7 @@ describe('Calculator', () => {
   })
 
     it ('should concatenate multiple clicks', () => {
-
+      // simultaneous button clicks generates concatenation 
       const button5 = container.find('#number5')
       const button4 = container.find('#number4')
       const button3 = container.find('#number3')
@@ -105,10 +105,64 @@ describe('Calculator', () => {
       button3.simulate('click')
       button2.simulate('click')
       button1.simulate('click')
-
+      // 54321 expected from multiple clicks
       expect(runningTotal.text()).toEqual('54321')
 
+    })
 
+    it ('should chain multiple operations', () => {
+      // adding multiple operations
+      const button6 = container.find('#number6')
+      const button5 = container.find('#number5')
+      const button2 = container.find('#number2')
+      const button1 = container.find('#number1')
+      const button0 = container.find('#number0')
+      const add = container.find('#operator_add')
+      const multiply = container.find('#operator-multiply');
+      const equals = container.find('#operator-equals')
+
+      const runningTotal = container.find('#running-total');
+
+      button6.simulate('click')
+      add.simulate('click')
+      button5.simulate('click')
+      button1.simulate('click')
+      multiply.simulate('click')
+      button2.simulate('click')
+      button0.simulate('click')
+      equals.simulate('click')
+
+      expect(runningTotal.text()).toEqual('1140')
+      
+
+    })
+
+    it ('should clear the running total', () => {
+      // clears the running total
+      const button7 = container.find('#number7')
+      const button6 = container.find('#number6')
+      const button5 = container.find('#number5')
+      const button4 = container.find('#number4')
+      
+      const clear = container.find('#clear')
+      const add = container.find('#operator_add')
+      const multiply = container.find('#operator-multiply');
+      const equals = container.find('#operator-equals')
+
+      const runningTotal = container.find('#running-total');
+
+      button7.simulate('click')
+      add.simulate('click')
+      button6.simulate('click')
+      multiply.simulate
+      button5.simulate('click')
+      add.simulate('click')
+      button4.simulate('click')
+      clear.simulate('click')
+      equals.simulate('click')
+      
+
+      expect(runningTotal.text()).toEqual('72')
 
 
 
