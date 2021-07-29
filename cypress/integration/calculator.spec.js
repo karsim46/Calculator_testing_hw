48c9a121c85e4f4025b62b0dca50f8419da38afa
Simon <simonwatson176@gmail.com>
2021-07-29 09:09:15 +0100

    4 ui tests passing

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -7,4 +7,38 @@ describe("Calculator", () => {
     cy.get('#number2').click();
     cy.get('.display').should('contain', '2')
   })
+
+  it('should update the display of running total', () => {
+    cy.get('#number3').click();
+    cy.get('#number9').click();
+    cy.get('.display').should('contain', '39')
+  })
+
+  it('should update the total', () => {
+
+    cy.get('#number5').click();
+    cy.get('#operator_add').click();
+    cy.get('#number6').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '11')
+    
+  })
+
+  it('should do multiple operations', () => {
+
+    cy.get('#number3').click();
+    cy.get('#number9').click();
+    cy.get('#operator_add').click();
+    cy.get('#number7').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number2').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', 92)
+
+
+  })
+
+
+
+
 })
